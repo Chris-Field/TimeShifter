6afbfd55eb1ca4e2a4ab515fd789714efe5951b7
drummerof13 <drummerof13@gmail.com>
2023-05-22 06:17:09 -0500

    fix: link more  CSS to app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { loadTaskArray } from '../../task-array';
 import { Calendar } from '../Calendar';
-// import { Calendar } from '../Calendar';
 
 export const App = () => {
   const [nav, setNav] = useState(0);
   const [clicked, setClicked] = useState();
   const [tasks, setTasks] = useState([]);
+  const [days, setDays] = useState([]);
   const [dateDisplay, setDateDisplay] = useState('');
   const [events, setEvents] = useState(loadTaskArray());
 
@@ -22,7 +22,7 @@ export const App = () => {
 
   return (
     <div className='main'>
-    <>Hello from the App component!</>
+      <>Hello from the App component!</>
       <Calendar />
     </div>
   );
