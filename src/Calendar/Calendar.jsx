2a716e2095a4e8600e3631eb2c8b0342b3c6eab7
drummerof13 <drummerof13@gmail.com>
2023-06-19 07:03:12 -0500

    refactor: replaced calendar functionality w/ react

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,15 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Day } from './Day';
 import { Timeline } from './Timeline';
-// import { Timeline } from './Timeline';
-// import Timeline from './Timeline';
-// import Day from './Calendar';
 
 export const Calendar = () => {
   const [days, setDays] = useState([]);
+  const [dayLimit, setDayLimit] = useState(7);
+  const [firstDayOfWeek, setFirstDayOfweek] = useState(1);
+  const [todayDayOfWeek, setTodayDayOfWeek] = useState(
+    new Date().getDay() - (firstDayOfWeek % dayLimit)
+  );
 
-  const startingDay = 'Monday';
+  useEffect(() => {
+    const today = new Date();
+    const todayDayOfWeek = today.getDay() - (firstDayOfWeek % dayLimit);
 
+    const firstDayOfWeekDate = new Date();
+    firstDayOfWeekDate.setDate(firstDayOfWeekDate.getDate() - todayDayOfWeek);
+
+    const tmpDays = [];
+
+    for (let iDayOfWeek = 0; iDayOfWeek < dayLimit; iDayOfWeek++) {
+      const iDate = new Date(firstDayOfWeekDate);
+      iDate.setDate(iDate.getDate() + iDayOfWeek);
+
+      tmpDays.push({
+        dayNumber: iDayOfWeek,
+        dayName: getDayOfWeekName(iDayOfWeek, dayLimit, firstDayOfWeek),
+        events: getEventsForDate(),
+        isToday: isSameDate(iDate, today),
+        date: iDate,
+      });
+    }
+
+    setDays(tmpDays);
+  }, [dayLimit, firstDayOfWeek, todayDayOfWeek]);
+  
   return (
     <div className='calendar'>
       <>Hello from the Calendar component!</>
@@ -22,3 +47,55 @@ export const Calendar = () => {
     </div>
   );
 };
+
+/**
+ * Set the date numbers on the calendar
+ * @param {Number} dayLimit - User-specified number of days to show in the calendar week
+ * @param {Number} firstDayOfWeek - The first day of the week, in integer form (i.e. 0 to start the calendar with Sunday, 1 for Monday, etc.)
+ * @param {Number} todayDayOfWeek -
+ */
+// export function updateCalendarDayNumbers(
+//   dayLimit = 7,
+//   firstDayOfWeek = 0,
+//   todayDayOfWeek
+// ) {
+//   updateCalendarHTML(function (dayElement) {
+//     const dateDay = currDate.getDate();
+//     // Find the element for the day number
+//     const currDayNumber = dayElement.querySelector(`.date > .date-num`);
+//     // Update the day number on the calendar
+//     currDayNumber.innerHTML = dateDay;
+//     // Update the day id
+//     currDayNumber.id = zeroPaddedDate(currDate);
+
+//     // Move the date forward
+//     currDate = new Date(currDate.setDate(currDate.getDate() + 1));
+//   });
+// }
+
+/**
+ * Set the day names in the calendar heading
+ * @param {Number} currentDayOfWeek - The current day of the week, in integer form (i.e. 0 to start the calendar with Sunday, 1 for Monday, etc.)
+ * @param {Number} firstDayOfWeek - The first day of the week, in integer form (i.e. 0 to start the calendar with Sunday, 1 for Monday, etc.)
+ * @param {Number} dayLimit - User-specified number of days to show in the calendar week
+ */
+function getDayOfWeekName(currentDayOfWeek, dayLimit = 7, firstDayOfWeek = 0) {
+  const dayNames = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+  return dayNames[(currentDayOfWeek + firstDayOfWeek) % dayLimit];
+}
+
+function isSameDate(date1, date2) {
+  const d1 = new Date(date1);
+  const d2 = new Date(date2);
+
+  // Set the time portion of both dates to 0
+  d1.setHours(0, 0, 0, 0);
+  d2.setHours(0, 0, 0, 0);
+
+  // Compare the date portion of the modified dates
+  return d1.getTime() === d2.getTime();
+}
+
+function getEventsForDate(date) {
+  return null;
+}
